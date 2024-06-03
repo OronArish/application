b5aa370e70f81dc639779fed1afe3f46052bdb8b
OronArish <oarish96@gmail.com>
2024-06-03 17:44:09 +0300

    unit tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,12 +1,6 @@
 import pytest
 from app import app
 
-@pytest.fixture
-def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
-
 def test_homepage(client):
     response = client.get('/')
     assert response.status_code == 200
