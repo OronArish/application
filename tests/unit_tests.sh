b5da8d58bf99f60b5cffae8ad12535ae8d5d339d
OronArish <oarish96@gmail.com>
2024-06-05 11:30:31 +0300

    fix tests

--- tests/unit_tests.sh
+++ tests/unit_tests.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 set -e
+set -x  
 
 BASE_URL="http://nginx-container:80"  
 
 function check_website() {
     echo "Checking if the website is up..."
-    if curl -s --head --request GET "$BASE_URL" | grep "200 OK" > /dev/null; then
+    response=$(curl --head --request GET "$BASE_URL")
+    echo "Curl response: $response"
+    if echo "$response" | grep "200 OK" > /dev/null; then
         echo "The website is up and running."
     else
         echo "The website is not responding."
     fi
 }
 
-check_website()
+check_website
